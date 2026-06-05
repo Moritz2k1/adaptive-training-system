ada6bf982d121e76295b484612933d326b035907
Moritz2k1 <moritz.gutschi@outlook.com>
2026-06-05 19:36:41 +0200

    feat: Moved files to a better folder structure -> Going to refactor the HeartRateManager now so it can be changed later on to the PolarSDK in the worst case without a problem

---
+++ AdaptiveTrainingSystem/Services/CoreBluetoothService.swift
@@ -0,0 +1,7 @@
+//
+//  CoreBluetoothService.swift
+//  AdaptiveTrainingSystem
+//
+//  Created by Moritz Gutschi on 05.06.26.
+//
+
---
+++ AdaptiveTrainingSystem/Views/ZoneSelectionView.swift
@@ -0,0 +1,8 @@
+//
+//  ZoneSelectionView.swift
+//  AdaptiveTrainingSystem
+//
+//  Created by Moritz Gutschi on 05.06.26.
+//
+
+import SwiftUI
