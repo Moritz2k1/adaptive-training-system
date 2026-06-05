f666b9513bdf80ba9a79d8b9afbd55dc66da74cb
Moritz2k1 <moritz.gutschi@outlook.com>
2026-06-05 21:09:23 +0200

    feat: Added the actual models, which will be used later on

--- AdaptiveTrainingSystem/Models/TrainingZoneModel.swift
+++ AdaptiveTrainingSystem/Models/TrainingZoneModel.swift
@@ -21,4 +21,51 @@ struct TrainingZoneModel : Identifiable {
     func maxHeartRate(userMaxHeartRate: Int) -> Int {
         Int(Double(userMaxHeartRate) * maxPercent)
     }
+    
+    static let ZoneModels: [TrainingZoneModel] = [
+        // Zone 1
+        TrainingZoneModel(
+            id: 1,
+            name: "Zone 1",
+            description: "Recovery",
+            minPercent: 0.5,
+            maxPercent: 0.6
+        ),
+        
+        // Zone 2
+        TrainingZoneModel(
+            id: 2,
+            name: "Zone 2",
+            description: "Endurance",
+            minPercent: 0.6,
+            maxPercent: 0.7
+        ),
+        
+        // Zone 3
+        TrainingZoneModel(
+            id: 3,
+            name: "Zone 3",
+            description: "Aerobic",
+            minPercent: 0.7,
+            maxPercent: 0.8
+        ),
+        
+        // Zone 4
+        TrainingZoneModel(
+            id: 4,
+            name: "Zone 4",
+            description: "Threshold",
+            minPercent: 0.8,
+            maxPercent: 0.9
+        ),
+        
+        // Zone 5
+        TrainingZoneModel(
+            id: 5,
+            name: "Zone 5",
+            description: "Maximum",
+            minPercent: 0.9,
+            maxPercent: 1.0
+        ),
+    ]
 }
