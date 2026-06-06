b05bded3fb240aba4a6991397ba0693733a1d391
Moritz2k1 <moritz.gutschi@outlook.com>
2026-06-06 21:08:29 +0200

    feat: Finished UserModel by adding an initializer

--- AdaptiveTrainingSystem/Models/User.swift
+++ AdaptiveTrainingSystem/Models/User.swift
@@ -9,11 +9,19 @@ import SwiftData
 import Foundation
 
 // SwiftData model - stored locally in app's db
-
+@Model
 class User {
     var id: UUID
     var username: String
     var email: String
     var password: String
     var createdAt: Date
+    
+    init(username: String, email: String, password: String) {
+        self.id = UUID()
+        self.username = username
+        self.email = email
+        self.password = password
+        self.createdAt = Date()
+    }
 }
