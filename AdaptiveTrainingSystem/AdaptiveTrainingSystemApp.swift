caa029d7a3f19bed591bc60b87a8e60b59ca0a4c
Moritz2k1 <moritz.gutschi@outlook.com>
2026-06-06 21:54:23 +0200

    feat: Updated the main file (AdaptiveTrainingSystem) to createa a local database (container)

--- AdaptiveTrainingSystem/AdaptiveTrainingSystemApp.swift
+++ AdaptiveTrainingSystem/AdaptiveTrainingSystemApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct AdaptiveTrainingSystemApp: App {
+    
+    // Constants for local database
+    let container: ModelContainer
+    let authService: AuthService
+    
+    init() {
+        do {
+            // Set up local database
+            container = try ModelContainer(for: User.self)
+            authService = AuthService(modelContext: container.mainContext)
+        } catch {
+            fatalError("Failed to create database: \(error)")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(container)
+                .environmentObject(authService)
         }
     }
 }
